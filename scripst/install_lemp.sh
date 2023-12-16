cc1c378741ef246fe9eefc2234fba7c9fb00a3da
Ubuntu <ubuntu@ip-172-31-87-21.ec2.internal>
2023-12-16 10:45:11 +0000

    ---

---
+++ scripst/install_lemp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Mostrar vervose
+set -ex
+
+# Incorporamos variables.
+source .env
+
+# Actualizamos los repositorios.
+apt update
+
+# Instalamos nginx
+apt install nginx -y
+
+# Instalacion de paquetes: paquete php-fpm y php-mysql
+apt install php-fpm php-mysql -y
+
+#-------------------#
+# php-fpm permite mejorar el consumo de memoria con mucho tráfico, este se ejecutará como un servicio independiente de Nginx.
+# php-mysql permite a php interar con el sistema gestor de base de datos.
+#-------------------#
+
+# Copiamos el contenido de default de default a /etc/nginx/sites-available/
+cp ../conf/default /etc/nginx/sites-available/default
+
+# Reiniciamos el servicio de nginx
+systemctl restart nginx
+
+# Copiamos el contenido de php a html para comprobar que funciona.
+cp ../php/info.php /var/www/html
